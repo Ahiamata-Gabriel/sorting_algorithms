4bfc8dc575c852943e237b6c61da1688e1243c2a
Ahiamata-Gabriel <69096276+Ahiamata-Gabriel@users.noreply.github.com>
2022-08-12 12:55:23 +0000

    Bubble sort add swap function

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -1,20 +1,38 @@
-#include <stdio.h>
-#include <stdlib.h>
 #include "sort.h"
 
+/**
+ *swap - swaps two integers.
+ *@a: Pointer to the first integer.
+ *@b: Pointer tothe second integer.
+ *Return: Nothing.
+ */
+void swap(int *a, int *b)
+{
+    int temp = *a;
+    *a = *b;
+    *b = temp;
+}
+
+/**
+ *bubble_sort - Sorts an array of integers in ascending order
+ *@array: An array of integers to be sorted.
+ *@size: Size of the array.
+ *Return: Nothing.
+ */
 void bubble_sort(int *array, size_t size)
 {
-    size_t c, j;
-    int temp;
-    for (c = 0; c < size; c++)
+    size_t i, j;
+
+    if (array == NULL || size == 0)
+        return;
+
+    for (i = 0; i < size - 1; i++)
     {
         for (j = 0; j < size - 1; j++)
         {
             if (array[j] > array[j + 1])
             {
-                temp = array[j];
-                array[j] = array[j + 1];
-                array[j + 1] = temp;
+                swap(&array[j], &array[j + 1]);
                 print_array(array, size);
             }
         }
