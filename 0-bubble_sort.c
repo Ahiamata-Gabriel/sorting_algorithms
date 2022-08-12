09df68eaf1a32c4b7e65fff0504be7ba69b3a9ab
Daniel Okyere <daniel.kwame.okyere101@gmail.com>
2022-08-12 23:01:21 +0000

    bubble sort fixes

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -10,7 +10,7 @@ void bubble_sort(int *array, size_t size)
 {
 	size_t i, j, swap;
 
-	if (!array || size == 0)
+	if (!array || size < 2)
 		return;
 	for (i = 0; i < size - 1; i++)
 	{
@@ -28,6 +28,5 @@ void bubble_sort(int *array, size_t size)
 		}
 		if (swap == 0)
 			break;
-		print_array(array, size);
 	}
 }
