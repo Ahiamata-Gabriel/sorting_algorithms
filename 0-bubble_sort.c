b1a4c63c2ca5f32f250dd0a5cd926a3d7f737810
Daniel Okyere <daniel.kwame.okyere101@gmail.com>
2022-08-12 22:29:34 +0000

    bubble sort complete no errors

---
+++ 0-bubble_sort.c
@@ -0,0 +1,36 @@
+#include "sort.h"
+
+/**
+ * bubble_sort - implementation of bubble sort
+ *
+ * @array: array to be sorted
+ * @size: size of array to be sorted
+ */
+void bubble_sort(int *array, size_t size)
+{
+	size_t i, j, swap;
+
+	size_t ar_length = size;
+
+	if (!array || size == 0)
+		return;
+	for (i = 0; i < ar_length - 1; i++)
+	{
+		swap = 0;
+		for (j = 0; j < ar_length - 1; j++)
+		{
+			if (array[j] > array[j + 1])
+			{
+				array[j] = array[j] ^ array[j + 1];
+				array[j + 1] = array[j] ^ array[j + 1];
+				array[j] = array[j] ^ array[j + 1];
+				swap = 1;
+				print_array(array, size);
+			}
+		}
+		if (swap == 0)
+			break;
+		print_array(array, size);
+	}
+}
+
