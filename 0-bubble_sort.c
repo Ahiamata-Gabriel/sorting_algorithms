8fc55eefc72a058e3d594917021c669ec1d02166
Ahiamata-Gabriel <69096276+Ahiamata-Gabriel@users.noreply.github.com>
2022-08-12 21:09:19 +0000

    Bubble sort error correction

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -22,17 +22,16 @@ void swap(int *xp, int *yp)
 void bubble_sort(int *array, size_t size)
 {
 	size_t i, j;
-
-	if (array == NULL || size == 0)
-		return;
-
-	for (i = 0; i < size - 1; i++)
+	int temp;
+	for (i = 0; i < size; i++)
 	{
 		for (j = 0; j < size - 1; j++)
 		{
 			if (array[j] > array[j + 1])
 			{
-				swap(&array[j], &array[j + 1]);
+				temp = array[j];
+				array[j] = array[j + 1];
+				array[j + 1] = temp;
 				print_array(array, size);
 			}
 		}
