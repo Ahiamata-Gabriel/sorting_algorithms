7543be6343fc97a790046584e10044e6499470f8
Daniel Okyere <daniel.kwame.okyere101@gmail.com>
2022-08-12 22:55:24 +0000

    fixing outputs

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -10,14 +10,12 @@ void bubble_sort(int *array, size_t size)
 {
 	size_t i, j, swap;
 
-	size_t ar_length = size;
-
 	if (!array || size == 0)
 		return;
-	for (i = 0; i < ar_length - 1; i++)
+	for (i = 0; i < size - 1; i++)
 	{
 		swap = 0;
-		for (j = 0; j < ar_length - 1; j++)
+		for (j = 0; j < size - 1; j++)
 		{
 			if (array[j] > array[j + 1])
 			{
